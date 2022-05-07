6bebd4df6ec4bb9c0b87e994b3d0d1917654a749
Shyam Grover <post2shyam@gmail.com>
2022-05-07 15:41:19 +0530

    Initial commit

---
+++ src/main/kotlin/com/androidz/yonderradioz/YonderradiozApplication.kt
@@ -0,0 +1,11 @@
+package com.androidz.yonderradioz
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class YonderradiozApplication
+
+fun main(args: Array<String>) {
+	runApplication<YonderradiozApplication>(*args)
+}
---
+++ src/test/kotlin/com/androidz/yonderradioz/YonderradiozApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.androidz.yonderradioz
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class YonderradiozApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
