21c22f33def6179c24a00d559f532d9d277dadfd
Shyam Grover <post2shyam@gmail.com>
2022-05-07 23:27:21 +0530

    Port is picked from heroku system env

--- src/main/kotlin/com/androidz/yonderradioz/YonderradiozApplication.kt
+++ src/main/kotlin/com/androidz/yonderradioz/YonderradiozApplication.kt
@@ -1,5 +1,6 @@
 package com.androidz.yonderradioz
 
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
@@ -8,5 +9,10 @@ import org.springframework.boot.runApplication
 class YonderradiozApplication
 
 fun main(args: Array<String>) {
+    val springApplication: SpringApplication = SpringApplication(YonderradiozApplication::class.java)
+    val mutableMapOf = mutableMapOf<String, String>()
+    mutableMapOf["server.port"] = System.getenv()["port"].toString()
+
+    springApplication.setDefaultProperties(mutableMapOf.toMap())
     runApplication<YonderradiozApplication>(*args)
 }
