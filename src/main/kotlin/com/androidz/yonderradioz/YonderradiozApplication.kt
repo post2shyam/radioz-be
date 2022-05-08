213ae06107e7ed606712f3b2a4923d1b89b5e7d1
Shyam Grover <post2shyam@gmail.com>
2022-05-08 09:02:29 +0530

    Port is picked from heroku system env

--- src/main/kotlin/com/androidz/yonderradioz/YonderradiozApplication.kt
+++ src/main/kotlin/com/androidz/yonderradioz/YonderradiozApplication.kt
@@ -8,12 +8,16 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 class YonderradiozApplication
 
 fun main(args: Array<String>) {
-
     val springApplication = SpringApplication(YonderradiozApplication::class.java)
     val mutableMapOf = mutableMapOf<String, String>()
-    val portNumber = System.getenv()["port"].toString()
 
-    mutableMapOf["server.port"] = portNumber //8081
+    //Get port number from Heroku's System environment
+    val envPortNumber = System.getenv()["PORT"]
+    val portNumber = envPortNumber ?: "8080"  //if port info not present in system, default to 8080
+
+    //Refer: https://stackoverflow.com/questions/14322989/first-heroku-deploy-failed-error-code-h10
+    //Refer: https://www.baeldung.com/spring-boot-change-port
+    mutableMapOf["server.port"] = portNumber
     println("PortNumber = $portNumber")
     springApplication.setDefaultProperties(mutableMapOf.toMap())
     springApplication.run(*args)
